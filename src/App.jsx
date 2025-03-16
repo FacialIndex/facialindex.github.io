aebd8005c39a78c3bfc803e599e9f5275e6ca03c
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-03-16 14:46:05 +0700

    update path

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import RootLayout from "./layout"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { MDXProvider } from "@mdx-js/react"
-import { Suspense, lazy } from "react"
-import MdxProviderWrapper from "@/components/MdxProviderWrapper"
+import { Route, Routes } from "react-router-dom"
+import { lazy } from "react"
 
 import { Home } from "./routes/home/Home"
 const About = lazy(() => import("./routes/about/page.mdx"))
@@ -21,21 +19,17 @@ const Theme = lazy(() => import("./routes/theme/page.mdx"))
 
 function App() {
     return (
-        <MdxProviderWrapper>
-            {/* <Suspense fallback={<div>Loading...</div>}> */}
-            <Routes>
-                <Route element={<RootLayout />}>
-                    <Route index path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/markdown" element={<Markdown />} />
-                    <Route path="/sample" element={<Sample />} />
-                    <Route path="/table" element={<Table />} />
-                    <Route path="/theme" element={<Theme />} />
-                </Route>
-            </Routes>
-            {/* </Suspense> */}
-        </MdxProviderWrapper>
+        <Routes>
+            <Route element={<RootLayout />}>
+                <Route index path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/markdown" element={<Markdown />} />
+                <Route path="/sample" element={<Sample />} />
+                <Route path="/table" element={<Table />} />
+                <Route path="/theme" element={<Theme />} />
+            </Route>
+        </Routes>
     )
 }
 
--- src/layout.jsx
+++ src/layout.jsx
@@ -3,13 +3,16 @@ import { Outlet, Link } from "react-router-dom"
 import { Header } from "@/components/layout/header"
 import { Body } from "@/components/layout/body"
 import { Footer } from "@/components/layout/footer"
+import MdxProviderWrapper from "@/components/MdxProviderWrapper"
 
 export default function RootLayout() {
     return (
         <div dir="ltr">
             <Header />
             <Body>
-                <Outlet />
+                <MdxProviderWrapper>
+                    <Outlet />
+                </MdxProviderWrapper>
             </Body>
             <Footer />
         </div>
